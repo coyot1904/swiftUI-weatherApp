1fb7c94a97b398f8441bb2f0ae73e091982fc42c
Keyvan.Mozaffari <keyvan.mozaffari@vagani.com>
2024-01-03 14:06:12 +0300

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Neda Mohammadi on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
